30a1ddfc0ce44d48fae0cefa54f83335323ed0d8
LanaD-coder <c.wnt.nd1053@gmail.com>
2025-09-24 13:45:47 +0200

    Build frontend and prepare for Heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AdminDashboard from "./components/AdminDashboard";
 import Navbar from "./components/Navbar";
 import BookingSection from "./components/BookingSection";
@@ -10,38 +10,36 @@ import FooterSection from "./components/FooterSection";
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        {/* Video background */}
-        <video autoPlay loop muted className="background-video">
-          <source src="/assets/videos/background-better.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+    <div className="app-container">
+      {/* Video background */}
+      <video autoPlay loop muted className="background-video">
+        <source src="/assets/videos/background-better.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        {/* Page content */}
-        <div className="content">
-          <Navbar />
+      {/* Page content */}
+      <div className="content">
+        <Navbar />
 
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <ProfileSection />
-                  <ServicesSection />
-                  <BookingSection />
-                  <ContactModal />
-                </>
-              }
-            />
-            <Route path="/admin" element={<AdminDashboard />} />
-          </Routes>
-        </div>
-
-        {/* Sticky Footer */}
-        <FooterSection />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <ProfileSection />
+                <ServicesSection />
+                <BookingSection />
+                <ContactModal />
+              </>
+            }
+          />
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
       </div>
-    </Router>
+
+      {/* Sticky Footer */}
+      <FooterSection />
+    </div>
   );
 }
 
